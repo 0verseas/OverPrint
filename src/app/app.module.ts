8137a497dc9391adc89596f77c8028084687e019
hewen-lotus <lotus3698@gmail.com>
2019-07-30 16:26:54 +0800

    update route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,19 +9,30 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { SearchStudentPipe } from './search-student.pipe';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'list', component: ListComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    SearchStudentPipe
+    SearchStudentPipe,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     // import HttpClientModule after BrowserModule.
     HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
