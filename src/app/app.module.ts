2d9009c4b962a567d0e6af02520f244eaf8f6ca4
hewen-lotus <lotus3698@gmail.com>
2020-12-24 17:29:10 +0800

    關閉 route tracing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,7 +38,7 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      { enableTracing: false } // <-- debugging purposes only
     )
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
